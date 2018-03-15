00f9a515dc9f900b661f8afab67860d085945d41
rickparrish <github@rick.parrish>
2018-03-14 20:04:23 -0400

    Version bump for updated CSS

--- js/ftelnet-loader.rip.noxfer.js
+++ js/ftelnet-loader.rip.noxfer.js
@@ -1,1 +1,1 @@
-document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.rip.noxfer.min.js?v=2018-03-07" id="fTelnetScript"><\/script>');
+document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.rip.noxfer.min.js?v=2018-03-14" id="fTelnetScript"><\/script>');
