7bc023b15f646c6db29492d3e93902d48635c446
rickparrish <rickparrish@users.noreply.github.com>
2022-11-15 14:11:37 -0500

    Version bump

--- js/ftelnet-loader.norip.xfer.js
+++ js/ftelnet-loader.norip.xfer.js
@@ -1,1 +1,1 @@
-document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.norip.xfer.min.js?v=2022-11-09" id="fTelnetScript"><\/script>');
+document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.norip.xfer.min.js?v=2022-11-15" id="fTelnetScript"><\/script>');
