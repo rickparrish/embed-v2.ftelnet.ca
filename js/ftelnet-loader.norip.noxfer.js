aba2a65d5285a0e95353334f29b78ac3dd126c75
rickparrish <rickparrish@users.noreply.github.com>
2022-11-20 21:44:06 -0500

    Mouse mode changes cursor type

--- js/ftelnet-loader.norip.noxfer.js
+++ js/ftelnet-loader.norip.noxfer.js
@@ -1,1 +1,1 @@
-document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.norip.noxfer.min.js?v=2022-11-15" id="fTelnetScript"><\/script>');
+document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.norip.noxfer.min.js?v=2022-11-20" id="fTelnetScript"><\/script>');
