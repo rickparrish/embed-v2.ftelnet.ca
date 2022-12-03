c968007228ad57408b163ad4ff7fa9a9ee3c2171
rickparrish <rickparrish@users.noreply.github.com>
2022-12-03 09:36:42 -0500

    Persist selected screen size

--- js/ftelnet-loader.norip.noxfer.js
+++ js/ftelnet-loader.norip.noxfer.js
@@ -1,1 +1,1 @@
-document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.norip.noxfer.min.js?v=2022-12-02a" id="fTelnetScript"><\/script>');
+document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.norip.noxfer.min.js?v=2022-12-03" id="fTelnetScript"><\/script>');
