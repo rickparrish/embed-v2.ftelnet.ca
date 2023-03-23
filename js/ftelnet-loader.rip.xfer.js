4986cca86c58e0dc452eb4a259dbe3591e4c41be
rickparrish <rickparrish@users.noreply.github.com>
2023-03-23 09:12:38 -0400

    Version bump

--- js/ftelnet-loader.rip.xfer.js
+++ js/ftelnet-loader.rip.xfer.js
@@ -1,1 +1,1 @@
-document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.rip.xfer.min.js?v=2022-12-03" id="fTelnetScript"><\/script>');
+document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.rip.xfer.min.js?v=2023-03-23" id="fTelnetScript"><\/script>');
