0e6a898741bce1e486155f7ada1b9153ae2b39d2
rickparrish <rickparrish@users.noreply.github.com>
2023-09-09 11:37:47 -0400

    Decrease delay on file transfer intervals to speed them up. Skip building the DebugLine unless connection logging is enabled.

--- js/ftelnet-loader.rip.xfer.js
+++ js/ftelnet-loader.rip.xfer.js
@@ -1,1 +1,1 @@
-document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.rip.xfer.min.js?v=2023-03-23" id="fTelnetScript"><\/script>');
+document.write('<script src="//embed-v2.ftelnet.ca/ftelnet/ftelnet.rip.xfer.min.js?v=2023-09-09" id="fTelnetScript"><\/script>');
